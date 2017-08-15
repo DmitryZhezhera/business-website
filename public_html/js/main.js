b2ec9e8f77a7e2d2fc9bafe1c27db8e69e15e8c7
Dmitry Zhezhera <>
2017-08-15 22:11:48 +0300

    пробую адаптивность + transform

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,5 +1,20 @@
 
 $(document).ready(function () {
+    $('#burger').click(function(){
+        if($('#burger').attr('class')==='burger_on'){
+           
+           console.log("BURGER PRESSED",$('#burger').attr('class')); 
+           $('#burger').removeClass('burger_on');
+           $('nav').css('display','none');
+        }
+        else{
+            $('nav').css('display','block');
+            
+            console.log("BURGER PRESSED",$('#burger').attr('class')); 
+            $('#burger').attr('class','burger_on');
+        }        
+    });
+    
     $('.slider').slick({
         dots: true,
         infinite: true,
